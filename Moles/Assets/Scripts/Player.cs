d9234c96e18b82318e9c65b6e08c2a259fcaacef
hilenyx <hilencornejososa@gmail.com>
2023-04-12 15:35:49 +0200

    added hammer animation

--- Moles/Assets/Scripts/Player.cs
+++ Moles/Assets/Scripts/Player.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     private void Update()
     {
         RaycastHit hit;
@@ -14,6 +22,12 @@ public class Player : MonoBehaviour
             {
                 if (Input.GetMouseButtonDown(0))
                 {
+                    if (!anim.enabled)
+                    {
+                        anim.enabled = true;
+                    }
+                    transform.position = hit.collider.transform.position;
+                    anim.SetTrigger("Hit");
                     hit.collider.gameObject.GetComponent<Mole>().Stunned();
                     Debug.Log(hit.collider.gameObject.name + " was hit");
                 }
